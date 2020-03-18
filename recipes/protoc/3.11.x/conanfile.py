6d024b16b121aab40af47319454c6cadd8f41bd9
James Correnti <james@shift5.io>
2020-03-18 13:13:24 -0400

    Adding protoc 3.11.4

---
+++ recipes/protoc/3.11.x/conanfile.py
@@ -0,0 +1,77 @@
+import os
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
+from conans.tools import Version
+
+
+class ProtocConan(ConanFile):
+    name = "protoc"
+    description = "Protocol Buffers C/C++ Compiler - Google's data interchange format"
+    topics = ("conan", "protobuf", "protocol-buffers", "protocol-compiler", "serialization", "rpc", "protocol-compiler")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/protocolbuffers/protobuf"
+    license = "BSD-3-Clause"
+    exports_sources = ["CMakeLists.txt", "protoc.patch"]
+    generators = "cmake"
+    short_paths = True
+    settings = "os", "arch", "compiler", "build_type"
+    options = {"shared": [True, False], "with_zlib": [True, False], "fPIC": [True, False], "lite": [True, False]}
+    default_options = {"with_zlib": False, "shared": False, "fPIC": True, "lite": False}
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    @property
+    def _is_clang_x86(self):
+        return self.settings.compiler == "clang" and self.settings.arch == "x86"
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        extracted_folder = "protobuf-" + self.version
+        os.rename(extracted_folder, self._source_subfolder)
+
+    def configure(self):
+        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            del self.options.fPIC
+            compiler_version = Version(self.settings.compiler.version.value)
+            if compiler_version < "14":
+                raise ConanInvalidConfiguration("On Windows Protobuf can only be built with "
+                                           "Visual Studio 2015 or higher.")
+
+    def requirements(self):
+        self.requires.add("protobuf/{}@shift5/testing".format(self.version), private=False)
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["protobuf_BUILD_TESTS"] = False
+        cmake.definitions["protobuf_WITH_ZLIB"] = False
+        if self.settings.compiler == "Visual Studio":
+            cmake.definitions["protobuf_MSVC_STATIC_RUNTIME"] = "MT" in self.settings.compiler.runtime
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
+
+    def build(self):
+        tools.patch(base_path=self._source_subfolder, patch_file="protoc.patch")
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_id(self):
+        del self.info.settings.compiler
+        del self.info.settings.arch
+        self.info.include_build_settings()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs.sort(reverse=True)
+        self.cpp_info.names["cmake_find_package"] = "protoc"
+        self.cpp_info.names["cmake_find_package_multi"] = "protoc"
